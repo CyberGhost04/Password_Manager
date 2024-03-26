b349449920b32c58022fad4794584821f165e9f9
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-03-26 13:31:57 +0530

    storage in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './Components/NavBar'
 import Manager from './Components/Manager'
+import Glass from './Components/Glass'
 
 function App() {
 
+  let ans = "http://localhost:5173"
+
   return (
     <>
-      <NavBar/>
-      <Manager/>
-      my ui
+      {/* <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${ans + '/icons/wall3.jpg'})` }}>
+      <Glass />
+      </div> */}
+
+      <NavBar />
+      <Manager/> 
     </>
   )
 }
