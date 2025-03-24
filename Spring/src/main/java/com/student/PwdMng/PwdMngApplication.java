843c2bc4db394ebbfd6f0605a926adabdf365fa8
CyberGhost04 <sidhantbahuguna@gmail.com>
2025-03-24 18:30:18 +0530

    migrating to spring begins here

---
+++ Spring/src/main/java/com/student/PwdMng/PwdMngApplication.java
@@ -0,0 +1,13 @@
+package com.student.PwdMng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwdMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwdMngApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/com/student/PwdMng/PwdMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.PwdMng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwdMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
